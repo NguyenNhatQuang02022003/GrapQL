46a885878d7ef4c1540c68f7db8e0b89868dea4a
NguyenNhatQuang02022003 <nhtquangnguyn645@gmail.com>
2025-08-05 10:06:08 +0700

    Remove dist and node_modules from Git tracking

--- dist/resolvers/main.resolvers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rootResolver = void 0;
-// resolvers/index.ts
-const user_resolver_1 = require("./user.resolver");
-const book_resolver_1 = require("./book.resolver");
-exports.rootResolver = Object.assign(Object.assign({}, user_resolver_1.userresolve), book_resolver_1.bookResolver);
