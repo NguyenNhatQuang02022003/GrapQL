2e88640778efff48573f97dbeed6af1e9a4f9890
NguyenNhatQuang02022003 <nhtquangnguyn645@gmail.com>
2025-08-14 10:42:42 +0700

    fix seeds

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -10,12 +10,12 @@ async function main() {
   const users = [
     { name: 'Nguyen Van A', dob: new Date('1990-01-01'), email: 'a@example.com', password: '123456', role: UserRole.user },
     { name: 'Tran Thi B', dob: new Date('1992-02-15'), email: 'b@example.com', password: '123456', role: UserRole.admin },
-    { name: 'Le Van C', dob: new Date('1995-03-20'), email: 'c@example.com', password: '123456', role: UserRole.superadmin },
+    { name: 'Le Van C', dob: new Date('1995-03-20'), email: 'c@example.com', password: '123456', role: UserRole.admin },
     { name: 'Pham Thi D', dob: new Date('1988-11-05'), email: 'd@example.com', password: '123456', role: UserRole.user },
     { name: 'Hoang Van E', dob: new Date('1993-06-10'), email: 'e@example.com', password: '123456', role: UserRole.user },
     { name: 'Bui Thi F', dob: new Date('1991-07-12'), email: 'f@example.com', password: '123456', role: UserRole.admin },
     { name: 'Do Van G', dob: new Date('1996-08-18'), email: 'g@example.com', password: '123456', role: UserRole.user },
-    { name: 'Ngo Thi H', dob: new Date('1994-09-22'), email: 'h@example.com', password: '123456', role: UserRole.superadmin },
+    { name: 'Ngo Thi H', dob: new Date('1994-09-22'), email: 'h@example.com', password: '123456', role: UserRole.admin },
     { name: 'Nguyen Nhat Quang',dob: new Date('2003-02-02'),email: 'nhtquangnguyen645@gmail.com.com',password: '12345678',role: UserRole.superadmin,createdAt: new Date('2025-08-04T08:43:25.709Z'),updatedAt: new Date('2025-08-04T09:19:22.739Z')}
   ]
 
