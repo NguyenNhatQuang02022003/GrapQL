68a2a12ba8fc9625c10b520dba4a6cfab37ee19a
NguyenNhatQuang02022003 <nhtquangnguyn645@gmail.com>
2025-08-20 14:22:40 +0700

    update routes

--- src/routes/graphql.routes.ts
+++ src/routes/graphql.routes.ts
@@ -1,15 +1,24 @@
-import { RequestHandler } from 'express';
+import { Router } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from '../model/schema';
 import { getUserFromToken } from '../gql/auth/auth';
+import expressPlayground from 'graphql-playground-middleware-express';
 
-export const graphqlRoute: RequestHandler = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  const user = token ? getUserFromToken(token) : null;
+const router = Router();
 
-  return graphqlHTTP({
-    schema,
-    graphiql: true,
-    context: { user },
-  })(req, res);
-};  
+router.post(
+  '/',
+  (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    const user = token ? getUserFromToken(token) : null;
+
+    return graphqlHTTP({
+      schema,
+      context: { user },
+    })(req, res);
+  }
+);
+
+router.get('/', expressPlayground({ endpoint: '/graphql' }));
+
+export const graphqlRoute = router;
